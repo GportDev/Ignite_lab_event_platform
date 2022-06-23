ef2520ba127e3e070a45a9023878cafbbbc081e0
Gabriel Porteiro <gportdev@gmail.com>
2022-06-22 22:30:27 -0300

    [FEATURES]: Creation of video player and controlers, creation of lesson content

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import { gql, useQuery } from "@apollo/client";
-import { Event } from "./pages/Event";
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { client } from "./lib/apollo";
+import { Router } from "./Router";
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
 
 function App() {
-  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
+
 
   return (
     <>
-      <Event/>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
     </>
   )
 }
